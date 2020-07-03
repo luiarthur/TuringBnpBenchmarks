86c03b0bf8b8b8de8bee7f45b4a3278467b36f0d
Arthur Lui <luiarthur@gmail.com>
2020-07-02 20:02:32 -0700

    made scripts

--- src/dp-gmm/scripts/dp_crp_gmm_turing.jl
+++ src/dp-gmm/scripts/dp_crp_gmm_turing.jl
@@ -5,6 +5,7 @@ println("Last updated: ", Dates.now(), " (PT)")
 import Pkg; Pkg.activate("../../../")
 
 # Import Libraries
+using DynamicPPL
 using Turing
 using Turing: Variational
 import Turing.RandomMeasures.DirichletProcess
@@ -19,8 +20,8 @@ include(joinpath(@__DIR__, "../util/BnpUtil.jl"));
 
 # Directory where all simulation data are stored.
 data_dir = joinpath(@__DIR__, "../../data/sim-data")
-# path_to_data = joinpath(data_dir, "gmm-data-n50.json")
-path_to_data = joinpath(data_dir, "gmm-data-n200.json")
+path_to_data = joinpath(data_dir, "gmm-data-n50.json")
+# path_to_data = joinpath(data_dir, "gmm-data-n200.json")
 
 # Load data in JSON format.
 data = let
@@ -47,18 +48,27 @@ function counts_plot(x; density=false, color="C0", lw=4)
     plt.vlines(number, 0, count, color=color, lw=lw)
 end
 
+exp_num_clus(a, n) = a * log1p(n / a)
+counts_plot(rand(Poisson(exp_num_clus(0.5, length(y))), 100000), density=true);
+plt.xlabel("counts")
+plt.ylabel("probability")
+plt.title("prior distribution for alpha");
+
+# a = [0.1, 0.5, 1.0, 1.5, 2.0]
+# plt.scatter(a, exp_num_clus.(a, 50))
+
 # DP GMM model under CRP construction
 @model dp_gmm_crp(y) = begin
     nobs = length(y)
     
-    alpha ~ Gamma(1, 0.1)  # mean = a*b
+    alpha ~ Gamma(1, 1)  # mean = a*b
     rpm = DirichletProcess(alpha)
     
     # Base measure.
     H = arraydist([Normal(0, 3), InverseGamma(2, 0.05)])
     
     # Latent assignment.
-    z = tzeros(Int, nobs)
+    z = tzeros(Int, nobs) * 0.1
     
     # Locations and scales of infinitely many clusters.
     mu_sigma = TArray(Vector{Float64}, 0)
@@ -84,6 +94,42 @@ end
 end
 ;
 
+@model dp_gmm_crp_delayed(y, Kmax) = begin
+    nobs = length(y)
+    
+    # alpha ~ Gamma(1, 1)  # mean = a*b
+    alpha ~ Uniform(0, 0.5)
+    rpm = DirichletProcess(alpha)
+    
+    # Base measure.
+    H_mu = Normal(0, 3)
+    # H_sigma = InverseGamma(2, 0.01)
+    H_sigma = Uniform(0, 0.3)
+    
+    # Latent assignment.
+    z = tzeros(Int, nobs)
+    n = tzeros(Int, nobs)
+    K = 0
+
+    mu = tzeros(Float64, Kmax)
+    sigma = tzeros(Float64, Kmax)
+
+    for i in 1:nobs
+        z[i] ~ ChineseRestaurantProcess(rpm,  n)
+        n[z[i]] += 1
+
+        if z[i] > K
+            mu[z[i]] ~ H_mu
+            sigma[z[i]] ~ H_sigma
+            K += 1
+        end
+        y[i] ~ Normal(mu[z[i]], sigma[z[i]])
+    end
+
+    # for i in 1:nobs
+    # end
+end
+
 # Set random seed for reproducibility
 Random.seed!(0);
 
@@ -95,29 +141,72 @@ Random.seed!(0);
     n_samples = 1000
     iterations = burn + n_samples
 
-    sample(dp_gmm_crp(y), SMC(), iterations)
+    # sample(dp_gmm_crp_delayed(y, 20), SMC(1.0), iterations)
+    sample(dp_gmm_crp_delayed(y, 30), PG(10), iterations)
+    # sample(dp_gmm_crp_delayed(y, 15), SMC(), iterations)
+    # sample(dp_gmm_crp_delayed(y, 30), Gibbs(PG(20, :z), PG(10, :alpha, :mu, :sigma)), iterations)
+    # sample(dp_gmm_crp_delayed(y, 15), Gibbs(PG(20, :z), PG(10, :mu, :sigma)), iterations)
+    # sample(dp_gmm_crp_delayed(y, 30), Gibbs(MH(0.1, :alpha), PG(10, :z), PG(10, :mu, :sigma)), iterations)
+    
+    # sample(dp_gmm_crp(y), SMC(), iterations)
     # sample(dp_gmm_crp(y), IS(), iterations)
     # sample(dp_gmm_crp(y), Gibbs(PG(5, :z), PG(5, :mu_sigma)), iterations)
     # sample(dp_gmm_crp(y), PG(10), iterations)
 end;
 
-chain.name_map
-chain.value
-# mean(chain[:mu_sigma].value.data[:, [1,3,5,7,9,11], 1], dims=1)
+chain.value.data
 
-z = chain.value.data[end-burn+1:end, (end-length(y)+1):end, 1]
+# chain[:sigma]
+
+z = chain.value.data[burn+1:end, (end-length(y)+1):end, 1]
 nclus = [length(unique(z[i, :])) for i in 1:size(z, 1)];
+
 plt.figure(figsize=(12,4))
 plt.subplot(1, 3, 1)
 plt.plot(nclus);
+plt.xlabel("iteration")
+plt.ylabel("number of clusters")
+
 plt.subplot(1, 3, 2)
-counts_plot(nclus);
+counts_plot(nclus, density=true);
+plt.xlabel("number of clusters")
+plt.ylabel("probability")
+
 plt.subplot(1, 3, 3)
-counts_plot(z[end, :]);
+i = 1000
+counts_plot(z[i, :], density=true);
+plt.xlabel("cluster label for sample $(i)")
+plt.ylabel("probability")
 
-# chain.value.data[:, 4:10, 1]
+plt.tight_layout();
 
-# exp_num_clus(a, n) = a * log1p(n / a)
-# counts_plot(rand(Poisson(exp_num_clus(1.0, 200)), 100000), density=true);
+plt.figure(figsize=(12, 4))
+
+plt.subplot(1, 3, 1)
+plt.boxplot(coalesce.(chain[:mu].value.data[burn+1:end, :, 1], 0.0));
+foreach(line -> plt.axhline(line, ls=":"), data[:mu])
+plt.ylabel("mu")
+plt.xlabel("mixture component")
+
+plt.subplot(1, 3, 2)
+plt.boxplot(coalesce.(chain[:sigma].value.data[burn+1:end, :, 1], 0.0));
+foreach(line -> plt.axhline(line, ls=":"), data[:sig])
+plt.ylabel("sigma")
+plt.xlabel("mixture component")
+
+plt.subplot(1, 3, 3)
+plt.hist(Float64.(chain[:alpha].value.data[:, 1, 1]), density=true)
+plt.xlabel("alpha")
+plt.ylabel("density")
+
+plt.tight_layout();
+
+plt.scatter(coalesce.(chain[:sigma].value.data[burn+1:end, :, 1], 0.0),
+            coalesce.(chain[:mu].value.data[burn+1:end, :, 1], 0.0),
+            alpha=0.1, label="posterior samples");
+plt.scatter(data[:sig], data[:mu], marker="X", s=60, label="truth")
+plt.xlabel("sigma")
+plt.ylabel("mu")
+plt.legend(loc="upper left");
 
 
