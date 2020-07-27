155129a841c047d516dea59411f21830c9226f2c
Arthur Lui <luiarthur@gmail.com>
2020-07-26 21:18:39 -0700

    added some timings for GP

--- src/gp/util/parse_timings.py
+++ src/gp/util/parse_timings.py
@@ -57,7 +57,7 @@ def get_turing_gp_times(path):
     r = re.findall(r'(?<=Time:\s)\d+:\d+:\d+', nb_content)
     t = list(map(float, t))
     r = list(map(sanitize_hms, r))
-    return dict(model='dp_sb_gmm',
+    return dict(model='gp',
                 ppl='turing',
                 advi_compile=float(t[0] - r[0]),
                 hmc_compile=float(t[1] - r[1]),
@@ -66,23 +66,65 @@ def get_turing_gp_times(path):
                 hmc_run=r[1],
                 nuts_run=r[2])
 
+def get_pyro_gp_times(path):
+    nb_content = read_file(path)
+    t = re.findall(r'(?<=\[)\d+[:\d+]+', nb_content)
+    t = list(map(sanitize_hms, t))
+    print(t)
+    return dict(model='gp',
+                ppl='pyro',
+                advi_compile=np.nan,
+                hmc_compile=0,
+                nuts_compile=0,
+                advi_run=np.nan,
+                hmc_run=t[0],
+                nuts_run=t[1])
+
+def get_numpyro_gp_times(path):
+    nb_content = read_file(path)
+    r = re.findall(r'(?<=\[)\d+[:\d+]+', nb_content)
+    t = re.findall(r'(?<=Wall time:\s).*(?=\\n)', nb_content)
+    r = list(map(sanitize_hms, r))
+    t = list(map(sanitize_py, t))
+    return dict(model='gp',
+                ppl='numpyro',
+                advi_compile=np.nan,
+                hmc_compile=t[0] - r[0],
+                nuts_compile=t[1] - r[1],
+                advi_run=np.nan,
+                hmc_run=r[0],
+                nuts_run=r[1])
+
+
 if __name__ == '__main__':
     path_to_nb="../notebooks"
     times_df = pd.DataFrame()
 
-    # GP STAN Timings.
+    # STAN GP Timings.
     stan_nb_path = '{}/gp_stan.ipynb'.format(path_to_nb)
     times = get_stan_gp_times(stan_nb_path)
     times_df = times_df.append(times, ignore_index=True)
-    print(times_df)
 
-    # Turing STAN Timings.
+    # Turing GP Timings.
     turing_nb_path = '{}/gp_turing.ipynb'.format(path_to_nb)
     times = get_turing_gp_times(turing_nb_path)
     times_df = times_df.append(times, ignore_index=True)
-    print(times_df)
 
-    # Pyro STAN Timings.
-    # Numpyro STAN Timings.
-    # TFP STAN Timings.
-    # NIMBLE STAN Timings.
+    # Pyro GP Timings.
+    pyro_nb_path = '{}/gp_pyro.ipynb'.format(path_to_nb)
+    times = get_pyro_gp_times(pyro_nb_path)
+    times_df = times_df.append(times, ignore_index=True)
+
+    # Numpyro GP Timings.
+    numpyro_nb_path = '{}/gp_numpyro.ipynb'.format(path_to_nb)
+    times = get_numpyro_gp_times(numpyro_nb_path)
+    times_df = times_df.append(times, ignore_index=True)
+
+    # TFP GP Timings.
+    # NIMBLE GP Timings.
+
+    # Save CSV
+    times_df.round().to_csv('../timings/timings.csv', index=False, na_rep='NA')
+
+    print(times_df)
+    print('Written to ../timings/timings.csv')
