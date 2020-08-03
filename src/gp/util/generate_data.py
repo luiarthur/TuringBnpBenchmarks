d16d289276fe8fe6e8fa91837abd0b7b9fd7b4d4
Arthur Lui <luiarthur@gmail.com>
2020-08-03 16:07:34 -0700

    made two larger datasets

--- src/gp/util/generate_data.py
+++ src/gp/util/generate_data.py
@@ -8,31 +8,38 @@ np.random.seed(1)
 def f(x):
     return np.sin(3 * x) * np.sin(x) * (-1)**(x > 0)
 
-# Number of observations.
-N = 30
+def make_data(nobs, ngrid=1000):
+    """
+    nobs (int): number of observations.
+    ngrid (int): number of grid points in fine grid for plotting true function.
+    """
+    # Predictors (between -3 and 3).
+    x = np.random.rand(nobs) * 6 - 3
 
-# Predictors (between -3 and 3).
-x = np.random.rand(N) * 6 - 3
+    # Response.
+    y = f(x)
 
-# Response.
-y = f(x)
+    # Finer grid for plotting true function.
+    x_true = np.linspace(-3.5, 3.5, ngrid)
+    f_true = f(x_true)
 
-# Finer grid for plotting true function.
-x_true = np.linspace(-3.5, 3.5, 1000)
-f_true = f(x_true)
+    # Create output
+    output = dict(x=x.round(3).tolist(),
+                  f=y.round(3).tolist(),
+                  x_true=x_true.round(3).tolist(),
+                  f_true=f_true.round(3).tolist())
 
-# Create output
-output = dict(x=x.round(3).tolist(),
-              f=y.round(3).tolist(),
-              x_true=x_true.round(3).tolist(),
-              f_true=f_true.round(3).tolist())
+    return output
 
 # Save data
-save_path = '../data/gp-data-N{}.json'.format(N) 
-with open(save_path, 'w') as f:
-    json.dump(output, f)
+for nobs in (30, 100, 300):
+    output = make_data(nobs)
+    save_path = '../data/gp-data-N{}.json'.format(nobs)
+    with open(save_path, 'w') as outfile:
+        json.dump(output, outfile)
 
-# Read data by:
-# with open(save_path, 'r') as f:
-#     data = json.loads(f.read())
+    print('Created data with {} observations.'.format(nobs))
 
+    # Read data by:
+    # with open(save_path, 'r') as f:
+    #     data = json.loads(f.read())
