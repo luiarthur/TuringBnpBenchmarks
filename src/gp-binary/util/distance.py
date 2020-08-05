dc5e21bd9b6b9f59bb036e5a1122adbaa6c66f02
Arthur Lui <luiarthur@gmail.com>
2020-08-04 20:29:02 -0700

    added python distance function

---
+++ src/gp-binary/util/distance.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+def squared_euclidean(X, Y):
+    return np.power(X[:, None, :] - Y[None, :, :], 2).sum(-1)
+
+def euclidean(X, Y):
+    return np.sqrt(squared_euclidean(X, Y))
+
+# TEST.
+# def slow(X, Y):
+#     M = X.shape[0]
+#     N = Y.shape[0]
+#     out = np.zeros((M, N))
+#     for m in range(M):
+#         for n in range(N):
+#             out[m, n] = np.power(X[m, :] - Y[n, :], 2).sum()
+#     return out
+# 
+# x = np.random.randn(20, 2)
+# y = np.random.randn(15, 2)
+# assert np.all(squared_euclidean(x, y) - slow(x, y) == 0)
+# euclidean(x, y)
