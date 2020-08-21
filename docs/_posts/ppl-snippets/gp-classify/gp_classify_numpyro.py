86fdbcc92dfeb0cca992ce8756e62fb60af00c47
Arthur Lui <luiarthur@gmail.com>
2020-08-21 16:51:44 -0700

    gp classifier snippets updated

--- docs/_posts/ppl-snippets/gp-classify/gp_classify_numpyro.py
+++ docs/_posts/ppl-snippets/gp-classify/gp_classify_numpyro.py
@@ -1,27 +1,6 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# <a href="https://colab.research.google.com/github/luiarthur/TuringBnpBenchmarks/blob/master/src/gp-classify/notebooks/gp_classify_numpyro.ipynb" target="_parent"><img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/></a>
-
-# In[1]:
-
-
-get_ipython().system('pip install numpyro;')
-
-
-# In[1]:
-
-
-get_ipython().system('echo "Last updated: `date`"')
-
-
-# In[3]:
-
-
+# Import libraries.
 import json
 import matplotlib.pyplot as plt
-import matplotlib as mpl
-mpl.rcParams['figure.dpi']= 150
 from jax import random, lax
 import jax.numpy as np
 import numpyro
@@ -36,109 +15,15 @@ from tqdm import trange
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import WhiteKernel, RBF
 
+# Default to double precision.
 numpyro.enable_x64()
 
 
-# In[4]:
-
-
-def plot_data(X, y, **kwargs):
-    colors = onp.array(['blue', 'red'])
-    plt.scatter(X[:, 0], X[:, 1], c=colors[y], **kwargs)
-
-
-def gen_grid(X, n, return_each=False, eps=0):
-    x0, x1 = np.meshgrid(np.linspace(X[:, 0].min()-eps, X[:, 0].max()+eps, n),
-                         np.linspace(X[:, 1].min()-eps, X[:, 1].max()+eps, n))
-    
-    Y = np.stack([x0.ravel(), x1.ravel()], axis=-1)
-    
-    if return_each:
-        return Y, x0, x1
-    else:
-        return Y
-
-
-# In[5]:
-
-
-def sample_posterior(guide, params, nsamples, seed=1):
-    samples = guide.get_posterior(params).sample(random.PRNGKey(seed), (nsamples, ))
-    # NOTE: Samples are arranged in alphabetical order.
-    #       Not in the order in which they appear in the
-    #       model. This is different from pyro.
-    return dict(alpha=onp.exp(samples[:, 0]),
-                beta=samples[:, 1],
-                eta=samples[:, 2:52],
-                rho=onp.exp(samples[:, -1]))
-
-sigmoid = lambda x: 1 / (1 + np.exp(-x))
-
-def gp_predict(X, Xnew, samples, i, eps=1e-6):
-    kernel = samples['alpha'][i]**2 * RBF(samples['rho'][i]) 
-    kernel += eps * WhiteKernel()
-    gpr = GaussianProcessRegressor(kernel=kernel, optimizer=None, random_state=0)
-    f = compute_f(samples['alpha'][i],
-                  samples['rho'][i],
-                  samples['beta'][i],
-                  samples['eta'][i], X)
-    gpr.fit(X, f)
-    return sigmoid(gpr.predict(Xnew))
-
-def plot_kernel_params(samples, algo, kernel_params=['alpha', 'rho', 'beta'],
-                       figsize=np.array([8, 3])):
-    plt.figure(figsize=figsize)
-    for i in range(len(kernel_params)):
-        plt.subplot(1, len(kernel_params), i + 1)
-        param = kernel_params[i]
-        plt.hist(samples[param], bins=30, density=True)
-        plt.xlabel(param)
-        plt.ylabel('density')
-        plt.title(f"Histograhm of {param} ({algo})")
-    plt.tight_layout()   
-
-def plot_uq(samples, X, nnew, algo, figsize=np.array([8, 3]), grid_eps=0.5):
-    Xnew, x0, x1 = gen_grid(X, nnew, return_each=True, eps=grid_eps)
-    preds = np.stack([gp_predict(X, Xnew, samples, i)
-                      for i in trange(samples['rho'].shape[0])])
-    plt.figure(figsize=figsize)
-    plt.subplot(1, 2, 1)
-    plt.contourf(x0, x1, preds.mean(0).reshape(x0.shape), 101,
-                cmap=plt.get_cmap('bwr'), vmin=0, vmax=1)
-    plt.title(f'Posterior Mean function ({algo})')
-    plt.xticks([]); plt.yticks([])
-    plt.colorbar()
-    plot_data(X, y, edgecolor="orange")
-
-    plt.subplot(1, 2, 2)
-    plt.contourf(x0, x1, preds.std(0).reshape(x0.shape), 101,
-                cmap=plt.get_cmap('Oranges'), vmin=0)
-    plt.title(f'Posterior SD function ({algo})')
-    plt.xticks([]); plt.yticks([])
-    plt.colorbar()
-    plot_data(X, y, edgecolor="black")
-    plt.tight_layout()
-
-    # Plot kernel parameters
-    plot_kernel_params(samples, algo, figsize=figsize)
-
-
-# ## Data
-
-# In[6]:
-
-
-# Make data
+### Make data ###
 X, y = make_moons(n_samples=50, shuffle=True, noise=0.1, random_state=1)
-plot_data(X, y)
 
 
-# ## Model
-
-# In[7]:
-
-
-# TODO: This is not efficient.
+### Define Model ###
 def sq_exp_cov(X, alpha, rho):
     D = np.linalg.norm(X[:, None] - X, ord=2, axis=-1)
     return alpha * alpha * np.exp(-0.5 * np.power(D / rho, 2))
@@ -166,62 +51,58 @@ def GPC(X, y):
     numpyro.sample('obs', dist.Bernoulli(logits=f), obs=y)
 
 
-# ## HMC
-
-# In[8]:
-
-
-get_ipython().run_cell_magic('time', '', '\n# Set random seed for reproducibility.\nrng_key = random.PRNGKey(0)\n\n# NOTE: num_leapfrog = trajectory_length / step_size\nhmc = MCMC(HMC(GPC, step_size=.05, trajectory_length=1,\n               adapt_step_size=False, adapt_mass_matrix=False),\n           num_samples=500, num_warmup=500)\nhmc.run(rng_key, X, y)\n\nhmc_samples = hmc.get_samples()')
-
-
-# In[9]:
-
-
-plot_uq(hmc_samples, X, 30, "HMC")
+### HMC ###
+# Set random seed for reproducibility.
+rng_key = random.PRNGKey(0)
+# NOTE: num_leapfrog = trajectory_length / step_size
+hmc = MCMC(HMC(GPC, step_size=.05, trajectory_length=1,
+               adapt_step_size=False, adapt_mass_matrix=False),
+           num_samples=500, num_warmup=500)
+hmc.run(rng_key, X, y)  # Run sampler
+hmc_samples = hmc.get_samples()  # Store samples.
 
 
-# ## NUTS
-
-# In[10]:
-
-
-get_ipython().run_cell_magic('time', '', '\n# Set random seed for reproducibility.\nrng_key = random.PRNGKey(0)\n\n# NOTE: num_leapfrog = trajectory_length / step_size\n\nnuts = MCMC(NUTS(GPC, target_accept_prob=0.8, max_tree_depth=10),\n            num_samples=500, num_warmup=500)\nnuts.run(rng_key, X, y)\n\nnuts_samples = nuts.get_samples()')
-
-
-# In[11]:
+### NUTS ###
+# Set random seed for reproducibility.
+rng_key = random.PRNGKey(0)
+nuts = MCMC(NUTS(GPC, target_accept_prob=0.8, max_tree_depth=10),
+            num_samples=500, num_warmup=500)
+nuts.run(rng_key, X, y)  # run sampler.
+nuts_samples = nuts.get_samples() ## collect samplers.
 
 
-plot_uq(nuts_samples, X, 30, "NUTS")
+### ADVI ###
+# Learn more about ADVI in Numpyro here: 
+#   http://num.pyro.ai/en/stable/svi.html
+
+# Compile
+guide = AutoDiagonalNormal(GPC)
+optimizer = numpyro.optim.Adam(step_size=0.01)
+svi = SVI(GPC, guide, optimizer, loss=ELBO())
+init_state = svi.init(random.PRNGKey(1), X, y)
+
+# Run optimizer for 1000 iteratons.
+state, losses = lax.scan(lambda state, i: 
+                         svi.update(state, X, y),
+                         init_state, np.arange(1000))
+
+# Extract surrogate posterior.
+params = svi.get_params(state)
+plt.plot(losses);
+
+def sample_posterior(guide, params, nsamples, seed=1):
+    samples = guide.get_posterior(params).sample(
+        random.PRNGKey(seed), (nsamples, ))
+    # NOTE: Samples are arranged in alphabetical order.
+    #       Not in the order in which they appear in the
+    #       model. This is different from pyro.
+    return dict(alpha=onp.exp(samples[:, 0]),
+                beta=samples[:, 1],
+                eta=samples[:, 2:52],
+                rho=onp.exp(samples[:, -1]))
+
+advi_samples = sample_posterior(guide, params,
+                                nsamples=500, seed=1)
 
 
-# ## ADVI
-# 
-# - Learn more about [ADVI in Numpyro][1]
-# 
-# [1]: http://num.pyro.ai/en/stable/svi.html
-# 
-# 
-
-# In[12]:
-
-
-get_ipython().run_cell_magic('time', '', '\n# Compile\nguide = AutoDiagonalNormal(GPC)\noptimizer = numpyro.optim.Adam(step_size=0.01)\nsvi = SVI(GPC, guide, optimizer, loss=ELBO())\ninit_state = svi.init(random.PRNGKey(1), X, y)')
-
-
-# In[13]:
-
-
-get_ipython().run_cell_magic('time', '', '\n# Run optimizer\nstate, losses = lax.scan(lambda state, i: \n                         svi.update(state, X, y), init_state, np.arange(1000))\n\n# Extract surrogate posterior.\nparams = svi.get_params(state)\nplt.plot(losses);\nadvi_samples = sample_posterior(guide, params, nsamples=500, seed=1)')
-
-
-# In[14]:
-
-
-plot_uq(advi_samples, X, 30, "ADVI")
-
-
-# In[14]:
-
-
-
-
+# NOTE: See notebook to see full example.
--- docs/_posts/ppl-snippets/gp-classify/gp_classify_stan.py
+++ docs/_posts/ppl-snippets/gp-classify/gp_classify_stan.py
@@ -1,97 +1,17 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-get_ipython().system('echo "Last updated: `date`"')
-
-
-# ## [GP for binary classification (in STAN)][1]
-# 
-# $$
-# \begin{eqnarray}
-# y_n \mid p_n &\sim& \text{Bernoulli}(p_n), \text{ for } n=1,\dots, N \\
-# \text{logit}(\mathbf{p}) &=& \mathbf{L} \cdot \boldsymbol{\eta} + \beta, \text{ where }
-# \mathbf{L} = \text{cholesky}(\mathbf{K}) \\
-# \eta_n &\sim& \text{Normal(0, 1)}, \text{ for } n=1,\dots,N \\
-# \beta &\sim& \text{Normal(0, 1)} \\
-# \alpha &\sim& \text{LogNormal}(0, 1) \\
-# \rho &\sim& \text{LogNormal}(0, 1) \\
-# \end{eqnarray}
-# %
-# $$
-# 
-# Note: 
-# - $\mathbf{K}$ is an $N\times N$ covariance matrix where
-#   $K_{n,m} = \alpha^2\exp\left(-{\left\lVert\mathbf{x}_n - \mathbf{x}_m\right\rVert}^2_2\big/\rho^{2}\right)$.
-# - Each $\mathbf{x}_n$ is an observed 2-dimensional locations, for $n=1,\dots,N$.
-# - The model above is the same as the more explicit model below. But the model above mixes slowly with ADVI, HMC, and NUTS; while the model above mixes much more quickly.
-# 
-# $$
-# \begin{eqnarray}
-# y_n \mid p_n &\sim& \text{Bernoulli}(p_n), \text{ for } n=1,\dots, N \\
-# \text{logit}(\mathbf{p}) &\sim& \text{MvNormal}(\beta \cdot \mathbf{1}_N, K) \\
-# \beta &\sim& \text{Normal(0, 1)} \\
-# \alpha &\sim& \text{LogNormal}(0, 1) \\
-# \rho &\sim& \text{LogNormal}(0, 1) \\
-# \end{eqnarray}
-# %
-# $$
-# 
-# [1]: https://mc-stan.org/docs/2_24/stan-users-guide/fit-gp-section.html
-
-# In[2]:
-
-
 # Load libraries.
 import json
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib as mpl
-mpl.rcParams['figure.dpi']= 150
 import pystan
 from tqdm import trange
 from scipy.spatial import distance_matrix
 from scipy.stats import norm
+from sklearn.datasets import make_moons
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import WhiteKernel, RBF
 
-import sys
-sys.path.append('../util')
-from pystan_vb_extract import pystan_vb_extract
 
-
-# *** 
-# Below are some helpers for plotting and generating data / grids.
-
-# In[3]:
-
-
-def gen_grid(X, n, return_each=False, eps=0):
-    x0, x1 = np.meshgrid(np.linspace(X[:, 0].min()-eps, X[:, 0].max()+eps, n),
-                         np.linspace(X[:, 1].min()-eps, X[:, 1].max()+eps, n))
-    
-    Y = np.stack([x0.ravel(), x1.ravel()], axis=-1)
-    
-    if return_each:
-        return Y, x0, x1
-    else:
-        return Y
-
-
-# In[4]:
-
-
-def plot_data(X, y, edgecolors=None):
-    colors = np.array(['blue', 'red'])
-    plt.scatter(X[:, 0], X[:, 1], color=colors[y], edgecolors=edgecolors)
-
-
-# In[5]:
-
-
-# Data dictionary.
+# Create data dictionary.
 def create_stan_data(X, y, m_rho=0, s_rho=1, m_alpha=0, s_alpha=1, eps=1e-6):
     N, P = X.shape
     assert (N, ) == y.shape
@@ -101,75 +21,7 @@ def create_stan_data(X, y, m_rho=0, s_rho=1, m_alpha=0, s_alpha=1, eps=1e-6):
                 m_alpha=m_alpha, s_alpha=s_alpha, eps=eps)
 
 
-# In[6]:
-
-
-sigmoid = lambda x: 1 / (1 + np.exp(-x))
-
-def gp_predict(X, Xnew, samples, i, eps=1e-6):
-    kernel = samples['alpha'][i]**2 * RBF(samples['rho'][i]) + eps*WhiteKernel()
-    gpr = GaussianProcessRegressor(kernel=kernel, optimizer=None, random_state=0)
-    gpr.fit(X, samples['f'][i, :])
-    return sigmoid(gpr.predict(Xnew))
-
-def plot_uq(samples, X, y, nnew, algo, eps=0.2, 
-            figsize1=(8, 3), figsize2=(8, 3),
-            color_res=100, color_res_sd=100, return_stuff=False,
-            data_edgecolors='gold', vmin_sd=0, vmax_sd=None,
-            kernel_params=['alpha', 'rho', 'beta']):
-    
-    nsamples = samples['alpha'].shape[0]
-    
-    nnew_rows = int(np.floor(np.sqrt(nnew)))
-    Xnew, x0, x1 = gen_grid(X, nnew_rows, return_each=True, eps=eps)
-    preds = np.stack([gp_predict(X, Xnew, samples, i) for i in trange(nsamples)]) 
-    
-    plt.figure(figsize=figsize1)
-    
-    # Plot mean function.
-    plt.subplot(1, 2, 1)
-    plt.contourf(x0, x1, preds.mean(0).reshape(x0.shape),
-                 color_res, cmap=plt.get_cmap('bwr'), vmin=0, vmax=1)
-    plt.colorbar();
-    plot_data(X, y, edgecolors=data_edgecolors);
-    plt.xticks([]); plt.yticks([]);
-    plt.title("Posterior Mean Function ({})".format(algo))
-    
-    # Plot function sd.
-    plt.subplot(1, 2, 2)
-    plt.contourf(x0, x1, preds.std(0).reshape(x0.shape),
-                 color_res_sd, cmap=plt.get_cmap('Oranges'),
-                 vmin=vmin_sd, vmax=vmax_sd)
-    plt.colorbar();
-    plot_data(X, y, edgecolors='black');
-    plt.xticks([]);
-    plt.yticks([]);    
-    plt.title("Posterior SD Function ({})".format(algo))
-    plt.tight_layout()
-    
-    # Plot kernel parameters.
-    plt.figure(figsize=figsize2)
-    for i in range(3):
-        param = kernel_params[i]
-        plt.subplot(1, 3, i+1)
-        plt.hist(samples[param], bins=30, density=True);
-        plt.title('Histogram of {}'.format(param))
-    plt.tight_layout()
-
-
-# ***
-
-# In[7]:
-
-
-# Set random seed for reproducibility.
-np.random.seed(1)
-
-
-# In[8]:
-
-
-# Process convolution model.
+# GP binary classification STAN model code.
 model_code = """
 data {
   int<lower=0> N;
@@ -228,104 +80,31 @@ model {
 """
 
 
-# In[9]:
-
-
 # Compile model. This takes about a minute.
-get_ipython().run_line_magic('time', 'sm = pystan.StanModel(model_code=model_code)')
+sm = pystan.StanModel(model_code=model_code)
 
 
-# In[10]:
-
-
-# Read data.
-path_to_data = '../data/gp-classify-data-N50.json'
-simdata = json.load(open(path_to_data))
-
-# Store data.
-X = np.stack([simdata['x1'], simdata['x2']], axis=-1)
-y = np.array(simdata['y'])
-
-# Plot data.
-plot_data(X, y)
-
-
-# In[11]:
-
+# Make data.
+X, y = make_moons(n_samples=50, shuffle=True, noise=0.1, random_state=1)
 
 # Generate stan data dictionary.
 stan_data = create_stan_data(X, y)
 
-
-# In[12]:
-
-
 # Fit via ADVI.
-get_ipython().run_line_magic('time', 'vb_fit = sm.vb(data=stan_data, iter=1000, seed=1, grad_samples=1, elbo_samples=1)')
-vb_samples = pystan_vb_extract(vb_fit)
+vb_fit = sm.vb(data=stan_data, iter=1000, seed=1,
+               grad_samples=1, elbo_samples=1)
 
-
-# In[13]:
-
-
-plot_uq(vb_samples, X, y, 500, "ADVI", eps=0.5)
-
-
-# ## HMC
-# 
+# Fit via HMC.
 # - stepsize = 0.05
 # - num leapfrog steps = 20
 # - burn in: 500
 # - samples: 500
-
-# In[14]:
-
-
-get_ipython().run_cell_magic('time', '', "\n# Fit via HMC.\nhmc_fit = sm.sampling(data=stan_data, iter=1000, warmup=500, thin=1,\n                      seed=1, algorithm='HMC', chains=1,\n                      control=dict(stepsize=0.05, int_time=1, adapt_engaged=False))")
-
-
-# In[15]:
+hmc_fit = sm.sampling(
+    data=stan_data, iter=1000, warmup=500, thin=1,
+    seed=1, algorithm='HMC', chains=1,
+    control=dict(stepsize=0.05, int_time=1, adapt_engaged=False))
 
 
-plot_uq(hmc_fit, X, y, 500, "HMC", eps=0.5)
-
-
-# In[16]:
-
-
-plt.plot(hmc_fit['lp__'])
-plt.xlabel('HMC iterations')
-plt.ylabel('log unnormalized joint density');
-
-
-# In[17]:
-
-
-get_ipython().run_cell_magic('time', '', "\n# Fit via NUTS.\nnuts_fit = sm.sampling(data=stan_data, iter=1000, warmup=500, thin=1,\n                       seed=1, algorithm='NUTS', chains=1)")
-
-
-# In[18]:
-
-
-plot_uq(nuts_fit, X, y, 500, "NUTS", eps=0.5)
-
-
-# In[19]:
-
-
-plt.plot(nuts_fit['lp__'])
-plt.xlabel('NUTS iterations')
-plt.ylabel('log unnormalized joint density');
-
-
-# In[20]:
-
-
-# nuts_fit.get_sampler_params()[0]['stepsize__'][-1]
-
-
-# In[ ]:
-
-
-
-
+# Fit via NUTS.
+nuts_fit = sm.sampling(data=stan_data, iter=1000, warmup=500, thin=1,
+                       seed=1, algorithm='NUTS', chains=1)
