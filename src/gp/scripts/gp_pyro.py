d35a54dcc506796640616cd9ccddba378cf81aef
Arthur Lui <luiarthur@gmail.com>
2020-08-22 13:38:05 -0700

    updated pyro example (advi works)

--- src/gp/scripts/gp_pyro.py
+++ src/gp/scripts/gp_pyro.py
@@ -7,7 +7,7 @@
 get_ipython().system('echo "Last updated: `date`"')
 
 
-# In[2]:
+# In[73]:
 
 
 # Libraries
@@ -24,7 +24,7 @@ from torch.nn.functional import pad
 from tqdm import trange
 
 # For ADVI
-from pyro.infer import SVI, Trace_ELBO, TraceEnum_ELBO
+from pyro.infer import SVI, Trace_ELBO, TraceEnum_ELBO, JitTrace_ELBO
 from pyro.contrib.autoguide import AutoDiagonalNormal
 from pyro.optim import Adam
 
@@ -35,7 +35,10 @@ import sys
 sys.path.append('../util')
 import gp_plot_util
 
-# See:
+# Default to double precision for torch objects.
+torch.set_default_dtype(torch.float64)
+
+# See also:
 # http://docs.pyro.ai/en/stable/contrib.gp.html
 # https://pyro.ai/examples/gp.html
 
@@ -61,41 +64,48 @@ plt.ylabel('y = f(x)')
 plt.legend();
 
 
-# In[4]:
+# In[70]:
 
 
-def make_gp_model(X, y,
-                  length_prior=dist.LogNormal(0.0, 1.0),
-                  variance_prior=dist.LogNormal(0.0, 0.1),
-                  noise_prior=dist.LogNormal(0.0, 1.0)):
+# Model definition.
+def sq_exp_kernel(d, alpha, rho):
+    return alpha * alpha * torch.exp(-0.5 * torch.pow(d / rho, 2))
+
+def sq_exp_kernel_matrix(X, alpha, rho):
+    D = torch.cdist(X, X)
+    return sq_exp_kernel(D, alpha, rho)
+
+def gp_model(X, y):
+    N = X.shape[0]
     
-    # Define squared exponential covariance function.
-    cov_fn = gp.kernels.RBF(input_dim=1)
-
-    # Define GP regression model.
-    gpr = gp.models.GPRegression(X, y, cov_fn)
-
-    # Place priors on GP covariance function parameters.
-    gpr.kernel.lengthscale = pyro.nn.PyroSample(length_prior)
-    gpr.kernel.variance = pyro.nn.PyroSample(variance_prior)
-    gpr.noise = pyro.nn.PyroSample(noise_prior)
+    # Priors for kernel parameters.
+    alpha = pyro.sample("alpha", dist.LogNormal(0., 0.1))
+    rho = pyro.sample("rho", dist.LogNormal(0., 1.))
+    sigma = pyro.sample("sigma", dist.LogNormal(0., 1.))
     
-    return gpr
+    # Covariance matrix. 
+    K = sq_exp_kernel_matrix(X, alpha, rho) + torch.eye(N) * sigma * sigma
+    L = torch.cholesky(K)
+    
+    # Marginal likelihood.
+    pyro.sample('obs',
+                dist.MultivariateNormal(torch.zeros(N), scale_tril=L),
+                obs=y)
 
 
-# In[5]:
+# In[64]:
 
 
-get_ipython().run_cell_magic('time', '', "\n### HMC ###\npyro.clear_param_store()\n\n# Set random seed for reproducibility.\npyro.set_rng_seed(1)\n\n# Make GP model for HMC\nhmc_gpr = make_gp_model(X, y)\n\n# Set up HMC sampler.\n# kernel = HMC(hmc_gpr.model, step_size=0.01, trajectory_length=1,\n#              adapt_step_size=True, adapt_mass_matrix=True)  # 14s\nkernel = HMC(hmc_gpr.model, step_size=0.01, trajectory_length=1,\n             adapt_step_size=False, adapt_mass_matrix=False, jit_compile=True)\nhmc = MCMC(kernel, num_samples=1000, warmup_steps=1000)\nhmc.run()\n\n# Get posterior samples\nhmc_posterior_samples = hmc.get_samples()\nhmc_posterior_samples = dict(rho=hmc_posterior_samples['kernel.lengthscale'].numpy(),\n                             alpha=hmc_posterior_samples['kernel.variance'].sqrt().numpy(),\n                             sigma=hmc_posterior_samples['noise'].sqrt().numpy())")
+get_ipython().run_cell_magic('time', '', '\n### HMC ###\npyro.clear_param_store()\n\n# Set random seed for reproducibility.\npyro.set_rng_seed(1)\n\n# Set up HMC sampler.\nhmc = MCMC(HMC(gp_model, step_size=0.01, trajectory_length=1,\n               adapt_step_size=False, adapt_mass_matrix=False,\n               jit_compile=True),\n           num_samples=1000, warmup_steps=1000)\nhmc.run(X, y)\n\n# Get posterior samples\nhmc_posterior_samples = hmc.get_samples()\nhmc_posterior_samples = {k: hmc_posterior_samples[k].numpy()\n                         for k in hmc_posterior_samples}')
 
 
-# In[6]:
+# In[65]:
 
 
-get_ipython().run_cell_magic('time', '', "\n### NUTS ###\npyro.clear_param_store()\n\n# Set random seed for reproducibility.\npyro.set_rng_seed(1)\n\n# Make GP model for NUTS\nnuts_gpr = make_gp_model(X, y)\n\n# Set up NUTS sampler.\nkernel = NUTS(nuts_gpr.model, target_accept_prob=0.8, jit_compile=True)\nnuts = MCMC(kernel, num_samples=1000, warmup_steps=1000)\n%time nuts.run()\n\n# Get posterior samples\nnuts_posterior_samples = nuts.get_samples()\nnuts_posterior_samples = dict(rho=nuts_posterior_samples['kernel.lengthscale'].numpy(),\n                              alpha=nuts_posterior_samples['kernel.variance'].sqrt().numpy(),\n                              sigma=nuts_posterior_samples['noise'].sqrt().numpy())")
+get_ipython().run_cell_magic('time', '', '\n### NUTS ###\npyro.clear_param_store()\n\n# Set random seed for reproducibility.\npyro.set_rng_seed(1)\n\n# Set up NUTS sampler.\nnuts = MCMC(NUTS(gp_model, target_accept_prob=0.8, jit_compile=True),\n            num_samples=1000, warmup_steps=1000)\n%time nuts.run(X, y)\n\n# Get posterior samples\nnuts_posterior_samples = nuts.get_samples()\nnuts_posterior_samples = {k: nuts_posterior_samples[k].numpy()\n                          for k in nuts_posterior_samples}')
 
 
-# In[7]:
+# In[66]:
 
 
 # Plot posterior for HMC
@@ -103,7 +113,7 @@ gp_plot_util.make_plots(hmc_posterior_samples, suffix="HMC",
                         x=X.flatten(), y=y, x_grid=x_grid, f=f, sigma_true=simdata['sigma'])
 
 
-# In[8]:
+# In[67]:
 
 
 # Plot posterior for NUTS
@@ -111,17 +121,17 @@ gp_plot_util.make_plots(nuts_posterior_samples, suffix="NUTS",
                         x=X.flatten(), y=y, x_grid=x_grid, f=f, sigma_true=simdata['sigma'])
 
 
-# In[9]:
+# In[91]:
 
 
-get_ipython().run_cell_magic('time', '', '\n### MAP ###\n# Can\'t get posterior samples for ADVI?\n\n# Clear parameter cache.\npyro.clear_param_store()\n\n# Set random seed for reproducibility.\npyro.set_rng_seed(3)\n\n# Make GP model for MAP\nmap_gpr = make_gp_model(X, y)\n\n# Find MAP estimator\noptimizer = torch.optim.Adam(map_gpr.parameters(), lr=5e-2)\nloss_fn = pyro.infer.Trace_ELBO().differentiable_loss\nlosses = []\nnum_steps = 500\nfor i in trange(num_steps):\n    optimizer.zero_grad()\n    loss = loss_fn(map_gpr.model, map_gpr.guide)\n    loss.backward()\n    optimizer.step()\n    losses.append(loss.item())\n    \n# Plot loss\nplt.plot(losses)\nplt.xlabel("Iteration")\nplt.ylabel("loss");\n\n# This \nmap_gpr.set_mode(\'guide\')\nmap_samples = dict(alpha=[map_gpr.kernel.variance.sqrt().item() for _ in range(10)],\n                   rho=[map_gpr.kernel.lengthscale.item() for _ in range(10)],\n                   sigma=[map_gpr.noise.sqrt().item() for _ in range(10)])')
+get_ipython().run_cell_magic('time', '', "# ADVI\npyro.clear_param_store()  # clear global parameter cache\npyro.set_rng_seed(1)  # set random seed\n\n# Automatically define variational distribution (a mean field guide).\nguide = AutoDiagonalNormal(gp_model)\n\n# Create SVI object for optimization.\nsvi = SVI(gp_model, guide, Adam({'lr': 1e-2}), JitTrace_ELBO())\n\n# Do 1000 gradient steps.\nadvi_loss = []\nfor step in trange(1000):\n    advi_loss.append(svi.step(X, y.double()))\n    \n# Bijector for advi samples.\ndef biject(samples):\n    return dict(alpha=samples[:, 0].exp().numpy(),\n                rho=samples[:, 1].exp().numpy(),\n                sigma=samples[:, 2].exp().numpy())\n\n# Get ADVI samples in constrained space.\nadvi_posterior_samples = biject(guide.get_posterior().sample((1000, )))   ")
 
 
-# In[10]:
+# In[79]:
 
 
-# Plot posterior for MAP
-gp_plot_util.make_plots(map_samples, suffix="MAP",
+# Plot posterior for ADVI
+gp_plot_util.make_plots(advi_posterior_samples, suffix="ADVI",
                         x=X.flatten(), y=y, x_grid=x_grid, f=f, sigma_true=simdata['sigma'])
 
 
