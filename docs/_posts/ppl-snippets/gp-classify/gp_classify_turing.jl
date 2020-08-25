2207b673f5acb6f4a48afc174fad4c5c6cd7dbd9
Arthur Lui <luiarthur@gmail.com>
2020-08-25 10:34:28 -0700

    cleaned up ppl snippet for turing

--- docs/_posts/ppl-snippets/gp-classify/gp_classify_turing.jl
+++ docs/_posts/ppl-snippets/gp-classify/gp_classify_turing.jl
@@ -29,49 +29,48 @@ end
     # Latent function.
     eta ~ filldist(Normal(0, 1), length(y))
     f = LinearAlgebra.cholesky(K).L * eta
-    # NOTE: The following more explicit form mixes very poorly.
-    # f ~ MvNormal(K)  # mean=0, covariance=K.
     
     # Sampling Distribution.
     y .~ Bernoulli.(logistic.(f .+ beta))
 end;
 
-# To extract parameters from ADVI model.
-function make_extractor(m, q; nsamples=300)
+# To extract parameters from trained variational distribution
+# (surrogate posterior).
+function make_surrogate_sampler(m, q; nsamples=300)
     qsamples = rand(q, nsamples)
     _, sym2range = Variational.bijector(m; sym_to_ranges=Val(true))
     return sym -> qsamples[collect(sym2range[sym][1]), :]
 end;
 
-# Read data.
+### Read data ###
+# See: https://github.com/luiarthur/TuringBnpBenchmarks/blob/master/src/gp-classify/data/gp-classify-data-N50.json
 data_path = joinpath(@__DIR__, "../data/gp-classify-data-N50.json")
+
 # Load data in JSON format.
 data = let
     x = open(f -> read(f, String), data_path)
     JSON3.read(x)
 end
 
-# Store data.
+### Name the data (X, y) ###
 X = [data["x1"] data["x2"]]
 y = Int64.(data["y"])
 
-# Create model.
+### Create model.###
 m =  GPClassify(Float64.(y), X);
 kernel_params = [:alpha, :rho, :beta];
 
-# Fit via ADVI.
+### Fit via ADVI ###
 Random.seed!(7)
-# initialize variational distribution (optional)
-q0 = Variational.meanfield(m)
 @time q = vi(m, ADVI(1, 1000))  # num_elbo_samples, num_iterations.
  
 # Get posterior samples
-extract_gp = make_extractor(m, q, nsamples=500)
-advi_samples = Dict{Symbol, Any}(sym => vec(extract_gp(sym)) for sym in kernel_params)
+surrogate_sampler = make_surrogate_sampler(m, q, nsamples=500)
+advi_samples = Dict{Symbol, Any}(sym => vec(surrogate_sampler(sym))
+                                 for sym in kernel_params)
 advi_samples[:eta] = extract_gp(:eta);
 
-
-# Fit via HMC.
+### Fit via HMC ###
 Random.seed!(0)
 burn = 500
 nsamples = 500
@@ -82,17 +81,16 @@ hmc_samples = Dict{Symbol, Any}([
     sym => vec(group(hmc_chain, sym).value.data)[end-nsamples+1:end]
 for sym in kernel_params])
 hmc_samples[:eta] = Matrix(group(hmc_chain, :eta).
-                           value.data[end-nsamples+1:end, :, 1]');
+                           value.data[end-nsamples+1:end, :, 1]')
 
-# Fit via NUTS.
+### Fit via NUTS ###
 Random.seed!(0)
 burn = 500
 nsamples = 500
-@time nuts_chain = sample(m, NUTS(burn, 0.8, max_depth=10), burn + nsamples);
+@time nuts_chain = sample(m, NUTS(burn, 0.8, max_depth=10), burn + nsamples)
 
 # Get posterior samples
 nuts_samples = Dict{Symbol, Any}([
-    sym => vec(group(nuts_chain, sym).value.data)[end-nsamples+1:end]
+    sym => vec(group(nuts_chain, sym).value.data)
 for sym in kernel_params])
-nuts_samples[:eta] = Matrix(group(nuts_chain, :eta).
-                            value.data[end-nsamples+1:end, :, 1]');
+nuts_samples[:eta] = Matrix(group(nuts_chain, :eta).value.data[:, :, 1]')
